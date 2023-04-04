8a9361b07968966547569619bad8a38328654630
carly <carly.cheung@solutionforest.net>
2023-04-04 18:30:59 +0800

    update config

--- config/filament-tree.php
+++ config/filament-tree.php
@@ -1,18 +1,20 @@
 <?php
 
 return [
+    /**
+     * Tree model fields
+     */
     'column_name' => [
         'order' => 'order',
         'parent' => 'parent_id',
         'depth' => 'depth',
     ],
+    /**
+     * Tree model default parent key
+     */
     'default_parent_id' => -1,
+    /**
+     * Tree model default children key name
+     */
     'default_children_key_name' => 'children',
-
-    'register' => [
-        /**
-         * Register filament-tree.css
-         */
-        'default_css' => true,
-    ]
 ];
