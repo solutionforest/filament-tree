b3372146572cd23fc930c002b21b11576c5c154b
aslanutku <138629898+aslanutku@users.noreply.github.com>
2023-11-05 09:04:30 +0300

    Add Turkish translation for filament-tree.php (lang-tr)

---
+++ resources/lang/tr/filament-tree.php
@@ -0,0 +1,31 @@
+<?php
+
+return [
+    'root' => 'Kök',
+    
+    /*
+        |--------------------------------------------------------------------------
+        | Buttons
+        |--------------------------------------------------------------------------
+    */
+    'button.save' => 'Kaydet',
+    'button.expand_all' => 'Tümünü Genişlet',
+    'button.collapse_all' => 'Tümünü Daralt',
+
+    /*
+        |--------------------------------------------------------------------------
+        | Form
+        |--------------------------------------------------------------------------
+    */
+    'components.tree.buttons.select_all.label' => 'Tümünü Seç',
+    'components.tree.buttons.deselect_all.label' => 'Seçimi Kaldır',
+    'components.tree.buttons.expand_all.label' => 'Tümünü Genişlet',
+    'components.tree.buttons.collapse_all.label' => 'Tümünü Daralt',
+    
+    /*
+        |--------------------------------------------------------------------------
+        | Message
+        |--------------------------------------------------------------------------
+    */
+    'actions.delete.confirmation.with_children' => 'Bu kaydı ve altındaki kayıtları silmek istediğinize emin misiniz?',
+];
