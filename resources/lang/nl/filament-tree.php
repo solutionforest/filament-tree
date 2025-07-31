c0939f96a907a6006a26462256533e7078c3a8ca
Sten Govaerts <sten.govaerts@gmail.com>
2025-07-31 12:21:19 +0200

    NL translations

---
+++ resources/lang/nl/filament-tree.php
@@ -0,0 +1,31 @@
+<?php
+
+return [
+    'root' => 'Basis',
+    
+    /*
+        |--------------------------------------------------------------------------
+        | Buttons
+        |--------------------------------------------------------------------------
+    */
+    'button.save' => 'Opslaan',
+    'button.expand_all' => 'Alles uitklappen',
+    'button.collapse_all' => 'Alles inklappen',
+
+    /*
+        |--------------------------------------------------------------------------
+        | Form
+        |--------------------------------------------------------------------------
+    */
+    'components.tree.buttons.select_all.label' => 'Alles selecteren',
+    'components.tree.buttons.deselect_all.label' => 'Alles deselecteren',
+    'components.tree.buttons.expand_all.label' => 'Alles uitklappen',
+    'components.tree.buttons.collapse_all.label' => 'Alles inklappen',
+    
+    /*
+        |--------------------------------------------------------------------------
+        | Message
+        |--------------------------------------------------------------------------
+    */
+    'actions.delete.confirmation.with_children' => 'Weet je zeker dat je dit record en bijbehorende onderliggende items wilt verwijderen?',
+];
