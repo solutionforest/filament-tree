bc971719caafa52944750238bb6856ee715e704f
Abdulbaki Suraj <surajabdulbaki19@gmail.com>
2025-08-20 17:21:27 +0100

    fix: replace blade component namespace with Filament 4 version

--- resources/views/components/modal/actions.blade.php
+++ resources/views/components/modal/actions.blade.php
@@ -1,7 +1,7 @@
-<x-filament-actions::actions
+<x-filament::actions
     :attributes="\Filament\Support\prepare_inherited_attributes($attributes)"
     :alignment="config('filament.layout.actions.modal.actions.alignment')"
     :dark-mode="config('filament.dark_mode')"
 >
     {{ $slot }}
-</x-filament-actions::actions>
+</x-filament::actions>
