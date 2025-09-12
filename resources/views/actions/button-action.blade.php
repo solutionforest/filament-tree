47d7e172dd8f25696922b09fcb7a594c3ffb134a
cklei-carly <carly.cheung@solutionforest.net>
2025-09-12 10:59:40 +0800

    Deprecate custom action views for Filament v4
    Replaced the contents of several custom action Blade views with deprecation comments, indicating they are no longer needed with Filament v4. This prepares the codebase for compatibility with the new version and signals to developers that these files are obsolete.

--- resources/views/actions/button-action.blade.php
+++ resources/views/actions/button-action.blade.php
@@ -1,12 +1,1 @@
-<x-filament-tree::actions.action
-    :action="$action"
-    dynamic-component="filament::button"
-    :outlined="$isOutlined()"
-    :labeled-from="$getLabeledFromBreakpoint()"
-    :icon-position="$getIconPosition()"
-    :icon-size="$getIconSize()"
-    class="filament-tree-button-action"
->
-    {{ $getLabel() }}
-</x-filament-tree::actions.action>
-
+{{-- Deprecated with filament v4 --}}
--- resources/views/actions/icon-button-action.blade.php
+++ resources/views/actions/icon-button-action.blade.php
@@ -1,6 +1,1 @@
-<x-filament-tree::actions.action
-    :action="$action"
-    :label="$getLabel()"
-    dynamic-component="filament::icon-button"
-    class="filament-tree-icon-button-action"
-/>
+{{-- Deprecated with filament v4 --}}
