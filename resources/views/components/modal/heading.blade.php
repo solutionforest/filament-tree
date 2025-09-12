f38aae5a9cb9419de6c7d99940917a3eee58ff0d
cklei-carly <carly.cheung@solutionforest.net>
2025-09-12 11:35:46 +0800

    Refactor modal components to use x-dynamic-component
    Replaces static Filament component tags with <x-dynamic-component> in modal-related Blade views. This change improves flexibility and consistency in how modal components are rendered and allows for easier customization and extension.

--- resources/views/components/modal/heading.blade.php
+++ resources/views/components/modal/heading.blade.php
@@ -1,6 +1,6 @@
-<x-filament::modal.heading
+<x-dynamic-component 
+    component="filament::modal.heading" 
     :attributes="\Filament\Support\prepare_inherited_attributes($attributes)"
-    :dark-mode="\Filament\Facades\Filament::hasDarkMode()"
 >
     {{ $slot }}
-</x-filament::modal.heading>
+</x-dynamic-component>
--- resources/views/components/modal/subheading.blade.php
+++ resources/views/components/modal/subheading.blade.php
@@ -1,6 +1,6 @@
-<x-filament::modal.description
+<x-dynamic-component 
+    component="filament::modal.description" 
     :attributes="\Filament\Support\prepare_inherited_attributes($attributes)"
-    :dark-mode="\Filament\Facades\Filament::hasDarkMode()"
 >
     {{ $slot }}
-</x-filament::modal.description>
+</x-dynamic-component>
